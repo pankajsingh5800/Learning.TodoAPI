40dc0d42787f1319ae274873ccd235fe27dc6dc7
Pankaj Singh <pankajsingh2018@hotmail.com>
2026-06-23 03:51:21 +0530

    Added Infrastructure Layer

---
+++ Todo.Infrastructure/Persistence/Entities/BaseAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Infrastructure.Persistence.Entities
+{
+    public abstract class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string? UpdatedBy { get; set; }
+    }
+}
---
+++ Todo.Infrastructure/Persistence/Entities/TodoItemTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Infrastructure.Persistence.Entities
+{
+    public class TodoItemTag
+    {
+        public Guid Id { get; set; }
+        public Guid TodoItemId { get; set; }
+        public TodoItem TodoItem { get; set; }
+
+        public Guid TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
