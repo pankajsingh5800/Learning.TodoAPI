96615079f845aa785d9ab87ac1539f6fe83b6fe4
Pankaj Singh <pankajsingh2018@hotmail.com>
2026-06-24 00:06:52 +0530

    Added Configuration

--- Todo.Infrastructure/Persistence/Entities/TodoAppDbContext.cs
+++ Todo.Infrastructure/Persistence/Entities/TodoAppDbContext.cs
@@ -12,6 +12,48 @@ namespace Todo.Infrastructure.Persistence.Entities
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoAppDbContext).Assembly);
+
+            modelBuilder.Entity<TodoList>()
+                .HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<TodoItem>()
+                .HasOne(x => x.TodoList)
+                .WithMany(x => x.TodoItems)
+                .HasForeignKey(x => x.TodoListId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(x => x.TodoItem)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.TodoItemId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<TodoItemTag>()
+                .HasOne(x => x.TodoItem)
+                .WithMany(x => x.TodoItemTags)
+                .HasForeignKey(x => x.TodoItemId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TodoItemTag>()
+                .HasOne(x => x.Tag)
+                .WithMany(x => x.TodoItemTags)
+                .HasForeignKey(x => x.TagId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
 
         public DbSet<User> Users { get; set; }
         public DbSet<TodoList> TodoLists { get; set; }
